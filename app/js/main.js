adb8ac48f63ace5ab96fe4f62867d037bb7d7304
serger777 <Serger777@gmail.com>
2015-10-29 16:53:30 +0300

    спрайты работают

--- app/js/main.js
+++ app/js/main.js
@@ -25,7 +25,8 @@ var main = (function(){
 	var _selects = function () {
 		$(".content-bar__select").select2({
 			placeholder: "Select a State",
-			allowClear: true
+			allowClear: true,
+			minimumResultsForSearch: Infinity
 		});
 	}
 	var _columnizer = function(){
@@ -44,23 +45,24 @@ var main = (function(){
 		$('#list-2').on('click', _sort_2);
 		$('#list-3').on('click', _sort_3);
 	};
+	/*--------------*/
 	var _sort_1 = function (){
 		$('#list-1').addClass('sort-bar__item_1_active');
 		$('#list-2').removeClass('sort-bar__item_2_active');
 		$('#list-3').removeClass('sort-bar__item_3_active');
-		$('#var').addClass('var-1').removeClass('var-2').removeClass('var-3')
+		$('#sel').addClass('sel-1').removeClass('sel-2').removeClass('sel-3')
 	}
 	var _sort_2 =  function (){
 		$('#list-2').addClass('sort-bar__item_2_active');
 		$('#list-1').removeClass('sort-bar__item_1_active');
 		$('#list-3').removeClass('sort-bar__item_3_active');
-		$('#var').addClass('var-2').removeClass('var-1').removeClass('var-3')
+		$('#sel').addClass('sel-2').removeClass('sel-1').removeClass('sel-3')
 	}
 	var _sort_3 = function (){
 		$('#list-3').addClass('sort-bar__item_3_active');
 		$('#list-2').removeClass('sort-bar__item_2_active');
 		$('#list-1').removeClass('sort-bar__item_1_active');
-		$('#var').addClass('var-3').removeClass('var-2').removeClass('var-1')
+		$('#sel').addClass('sel-3').removeClass('sel-2').removeClass('sel-1')
 	}
 	var _accordeonUp = function(e){
 		e.preventDefault();
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,18 @@ var
     compass     = require('gulp-compass'),
     jade        = require('gulp-jade'),
     browserSync = require('browser-sync').create(),
+    wiredep = require('wiredep').stream,
+    useref = require("gulp-useref"),
+    uglify = require("gulp-uglify"),
+    minifyCss = require("gulp-minify-css"),
+    gulpif = require("gulp-if"),
+    del = require("del"),
+    filter = require("gulp-filter"),
+    imagemin = require("gulp-imagemin"),
+    size = require("gulp-size"),
+    gutil = require("gulp-util"),
+    concatCss = require("gulp-concat-css"),
+    RS_CONF = require('./rs-conf.js'),
     plumber     = require('gulp-plumber');
 
 /* --------- paths --------- */
@@ -81,3 +93,85 @@ gulp.task('watch', function(){
 /* --------- default --------- */
 
 gulp.task('default', ['jade', 'compass', 'sync', 'watch']);
+
+/*******************************************
+ * DIST
+ ******************************************/
+
+gulp.task('jadeDist', function() {
+    gulp.src(paths.jade.compiled)
+        .pipe(plumber())
+        .pipe(jade({
+            pretty: '\t',
+        }))
+        .pipe(gulp.dest(paths.jade.destination));
+});
+
+//включаем bower файлы
+gulp.task('bowerDist', function () {
+    gulp.src(RS_CONF.path.htmlDir)
+        .pipe(wiredep({
+            derictory: RS_CONF.path.baseDir+"/bower",
+            overrides: {
+                "qtip2": {
+                    "main": ["./jquery.qtip.min.js", "./jquery.qtip.min.css"],
+                    "dependencies": {"jquery": ">=1.6.0"}
+                }
+                , exclude: ["bower/qtip2/"]
+                , ignorePath: /^(\.\.\/)*\.\./
+            }
+        }))
+        .pipe(gulp.dest(RS_CONF.path.baseDir));
+});
+
+gulp.task('watch-bower', function (){
+    gulp.watch('bower.json',['bower']);
+})
+
+// Переносим CSS JS HTML в папку DIST
+gulp.task("userefDist", function () {
+    var assets = useref.assets();
+    return gulp.src(RS_CONF.path.htmlDir)
+        .pipe(assets)
+        .pipe(gulpif("*.js", uglify()))
+        .pipe(gulpif("*.css", minifyCss({compatibility: "ie8"})))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('compassDist', function() {
+    gulp.src(paths.scss.location)
+        .pipe(plumber())
+        .pipe(compass({
+            config_file: paths.compass.configFile,
+            css: paths.compass.cssFolderDist,
+            sass: paths.compass.scssFolder,
+            image: paths.compass.imgFolder
+        }));
+});
+
+gulp.task('syncDist', function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.browserSync.baseDirDist
+        }
+    });
+});
+
+gulp.task('watchDist', function(){
+    gulp.watch(paths.browserSync.watchPaths).on('change', browserSync.reload);
+});
+
+// Перенос картинок
+gulp.task("images", function () {
+    return gulp.src(RS_CONF.path.baseDir+"/img/**/*")
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest(RS_CONF.path.distDir+"/img"));
+});
+
+gulp.task('build', ['jadeDist', 'bowerDist', 'userefDist', 'compassDist','images']);
+gulp.task('build-sync', ['jadeDist', 'bowerDist', 'userefDist', 'compassDist','images', 'syncDist', 'watchDist']);
